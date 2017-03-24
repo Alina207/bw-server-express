0ab09ef05f6985b8e4545a87efb786f8a143a83d
Alina Gasperino <alina.gasperino@gmail.com>
2017-03-24 12:19:01 -0400

    Setup passport & express-session

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const layouts      = require('express-ejs-layouts');
 const mongoose     = require('mongoose');
+const session      = require('express-session');
+const passport     = require('passport');
 
 
 mongoose.connect('mongodb://localhost/bw-server-express');
@@ -17,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Baitwatch';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,6 +29,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
+app.use(session({
+  secret: 'Its a secret shhh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const passportSetup = require('./config/passport');
+passportSetup(passport);
 
 const index = require('./routes/index');
 app.use('/', index);
