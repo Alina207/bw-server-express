cecc894bd5fba52e0f10707c16cd91ae46ad15c9
Alina Gasperino <alina.gasperino@gmail.com>
2017-04-06 11:28:57 -0400

    sendFile

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use('/api', articlesApi);
 
 // connects ang to express
 app.use((req, res, next) => {
-  res.sendfile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // catch 404 and forward to error handler
